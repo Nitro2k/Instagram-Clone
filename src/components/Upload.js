83e77bd87771bd275a808afbb6291a426adfa9e8
Nuttachat <Nuttachat.su@gmail.com>
2022-06-16 15:21:40 +0700

    add button and caption input

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
 import { Button } from 'antd'
 
 //styles
 import './Upload.css'
 
 export default function Upload() {
+  const [caption, setCaption] = useState('')
+
   const handleChange = () => {
     console.log('handleChange')
   }
+  const handleUpload = () => {
+    console.log('handleUpload')
+  }
   return (
     <div className="upload">
-      <input type="text" />
-      <input type="file" onChange={handleChange} />
-      <Button type="primary">Upload</Button>
+      <input className="upload_fie" type="file" onChange={handleChange} />
+      <input
+        className="upload_caption"
+        type="text"
+        placeholder="Enter a caption..."
+        value={caption}
+        onChange={(e) => setCaption(e.target.value)}
+      />
+      <Button type="primary" onClick={handleUpload}>
+        Upload
+      </Button>
     </div>
   )
 }
